c281e1f265a5bb3eb0cec82a5ee4e15fcb4c46e2
Alex Szczerba <Szczerba18@icloud.com>
2021-11-05 15:47:16 -0700

    replaced quotes
    replaced sports quotes with book quotes, updated html and array

--- js/script.js
+++ js/script.js
@@ -9,59 +9,74 @@ project 1 - A Random Quote Generator
 
 const quotes = [
   {
-    quote: "You miss 100% of the shots you don't take.",
-    source: 'Wayne Gretzky',
-    citation: 'NHL Legend',
-    year: '1991'
+    quote: "Forgiveness condones nothing, but it does cast off the chains of anger, judgment, resentment, denial, and pain that chokes growth. In this way, it allows for life, for freedom.",
+    source: 'The Beauty in Breaking',
+    citation: 'Michelle Harper',
+    year: '2020',
+    genre: 'Memoir',
   },
   {
-    quote: "There may be people that have more talent than you, but there's no excuse for anyone to work harder than you.", 
-    source: 'Derek Jeter', 
-    citation: 'MLB Legend',
-    year: '1996'
+    quote: "To choose doubt as a philosophy of life is akin to choosing immobility as a means of transportation.", 
+    source: 'Life of Pi', 
+    citation: 'Yann Martel',
+    year: '2001',
+    genre: 'Fiction',
   },
   {
-    quote: "The wall is your mind playing tricks on you. You just need to say, 'One more step, I can do this. I have more in me.' You'll be so proud of yourself once you push pass your threshold.", 
-    source: 'Kerry Walsh', 
-    citation: 'FIVB Legend',
+    quote: "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway.", 
+    source: 'Americanah', 
+    citation: 'Ngozi Adichie',
+    year: '2013',
+    genre: 'Fiction',
   },
   {
-    quote: 'If you do not believe in yourself no one will do it for you.', 
-    source: 'Kobe Bryant', 
-    citation: 'NBA Legend',
+    quote: 'When you start to love yourself for the first time, when you start to truly emrace who you are - flaws and all - your scars start to look a lot more like beauty marks', 
+    source: 'Sissy', 
+    citation: 'Jacob Tobia',
+    genre: 'Memoir',
   },
   {
-    quote: 'The greatest thing about tomorrow is, I will be better than I am today', 
-    source: 'Tiger Woods', 
-    citation: 'PGA Legend',
+    quote: `What's the point of having a voice if you're gonna be silent in those moments you shouldn't be?`, 
+    source: 'The hate you give', 
+    citation: 'Angie Thomas',
+    year: '2017',
   },
   {
-    quote: "Achievement is largely the product of steadily raising one's level of aspiration and expectation.",
-    source: 'Jack Nicklaus', 
-    citation: 'PGA Legend', 
-    
+    quote: "There is no greater agony than bearing an untold story inside you.",
+    source: 'I know why the caged bird sings', 
+    citation: 'Maya Angelou', 
+    year: '1969', 
   },
   {
-    quote: "It doesn't matter what your background is or where you come from, if you have dreams and goals, that's all that matters.",
-    source: 'Serena Williams', 
-    citation: 'WTA Legend',
-  }
-
+    quote: "You never really understand a person until you consider things from his point of view...Until you climb inside of his skin and walk around in it.",
+    source: 'To kill a Mockingbird', 
+    citation: 'Harper Lee',
+    year:'1960',
+    genre:'Fiction' 
+  },
+  {
+    quote:`When you make loving others the story of your life, there's never a final chapter, because the legacy continues. You lend your light to one person, and he or she shines it on another and another and another.`,
+    source:'What I know for sure', 
+    citation:'Oprah Winfrey',
+    year:'2014',
+},
 ];
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` uses random number generator * length of quotes array to return 1 object from quotes array.
 ***/
-
-function getRandomQuote () {
+function getRandomQuote() {
   let randomQuote = Math.floor(Math.random() * quotes.length);
   return quotes[randomQuote];
 } 
 
-/***
- * `printQuote` function
-***/
+function getColor() {
 
+
+}
+/***
+ * `printQuote` function displays a random quote via `getRandomQuote` function 
+***/
 function printQuote() {
   let phrase = getRandomQuote(); 
   let html = `
@@ -76,14 +91,19 @@ function printQuote() {
   if (phrase.year) {
     html += `<span class='year'>${phrase.year}</span>`;
   }
+  if (phrase.genre) {
+    html += `<span class='genre'>, ${phrase.genre}</span>`;
+  }
     `</p>`
- 
+ // displays new HTML inside 'quote-box'
     document.getElementById('quote-box').innerHTML = html;
   }
+// timers to get new quote/new color every 5000 miliseconds
+  setInterval(printQuote, 10000);
+  setInterval(getColor, 10000);
 
-  setInterval(printQuote, 5000);
-/***
- * click event listener for the print quote button
+  /***
+* click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
